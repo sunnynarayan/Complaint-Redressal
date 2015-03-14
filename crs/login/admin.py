10e1a28a9569f63d89a26491799d872be5d907b1
ankit <ankit.cs13@iitp.ac.in>
2015-03-15 03:21:09 +0530

    admin  panel added

--- crs/login/admin.py
+++ crs/login/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from login.models import *
+from secretary.models import *
+from student.models import *
+from warden.models import *
+from wardenOffice.models import *
 
-# Register your models here.
+admin.site.register(Clink)
+admin.site.register(Com)
+admin.site.register(Pollque)
+admin.site.register(Pollres)
+admin.site.register(AuthGroup)
+admin.site.register(AuthGroupPermissions)
+admin.site.register( AuthUserUserPermissions)
+admin.site.register(Complain)
+admin.site.register(Complainlink)
+admin.site.register(DjangoAdminLog)
+admin.site.register(DjangoContentType)
+admin.site.register(DjangoMigrations)
+admin.site.register(DjangoSession)
+admin.site.register(Faculty)
+admin.site.register(Hostel)
+admin.site.register(Secretary)
+admin.site.register(Student)
+admin.site.register(Warden)
+
+

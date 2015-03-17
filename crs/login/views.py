6af1e53b8c2c37bfa8502f8703651e79adf42fe9
ankit <ankit.cs13@iitp.ac.in>
2015-03-17 18:28:39 +0530

    Changes in login.views

--- crs/login/views.py
+++ crs/login/views.py
@@ -22,19 +22,19 @@ def validatePassword(passwd):
 
 def login(request):
 	try:
-		if request.session.get("login") == "True":							#check if the user is already logged in
-			if request.session.get("user_type")=="faculty" :		#if yes then redirect the request to home page according to whether faculty or student
-				if request.session.get("username") == "nalin":
-					return render_to_response('wardenOffice/wardenHome.html', {'msg' : 'Nalin Bharti'});
-				else :
-					return render_to_response('warden/wardenBase.html', {'msg' : request.session.get('name')})
+		if request.session.get("login") == "True": #check if the user is already logged in
+			if request.session.get("user_type")=="wardenOffice" : #if yes then redirect the request to home page according to whether faculty or student
+				return render_to_response('wardenOffice/wardenHome.html', {'msg' : request.session.get('name')});
+			elif request.session.get("user_type")=="warden":
+				return render_to_response('warden/wardenBase.html', {'msg' : request.session.get('name')})
 			elif request.session.get("user_type")=="secretary" :
 				return render_to_response('secretary/secHome.html', {'msg' : request.session.get('name')});
 			else:
 				return render_to_response('student/studentBase.html', {'msg' : request.session.get('name')});
 	except NameError:
-		return render_to_response('login/loginPage.html', {'msg':''});	
-	return render_to_response('login/loginPage.html', {'msg':''});		#if not then display the login page
+		pass
+	return render_to_response('login/loginPage.html', {'msg':''}); #if not then display the login page
+
 
 def afterLogin(request):								#after login function working
 	uname = request.POST.get('username','');
@@ -54,11 +54,13 @@ def afterLogin(request):								#after login function working
 			request.session['login']="True";
 			request.session['username']=uname;
 			request.session['name'] = obj.name;
-			request.session['user_type']="faculty";
+			# request.session['user_type']="faculty";
 			request.session['uid']= obj.fid;
-			if uname=='nalin':
+			if obj.iswarden == 2:
+				request.session['user_type']="wardenOffice";
 				return render_to_response('wardenOffice/wardenHome.html', {'msg' : obj.name});
-			else:
+			elif obj.iswarden == 1:
+				request.session['user_type']="warden";
 				return render_to_response('warden/wardenBase.html', {'msg' : obj.name})
 		except:
 			return render_to_response('login/loginPage.html', {'msg':'invalid user: '+uname + 'password : ' + passwd});
@@ -80,9 +82,10 @@ def afterLogin(request):								#after login function working
 				return render_to_response('student/studentBase.html', {'msg':obj.name});
 
 		except:
-			return render_to_response('login/loginPage.html', {'msg' : obj.issec});
+			return render_to_response('login/loginPage.html');
 	else:
 		return render_to_response('login/loginPage.html', {'msg' : 'username recieved(line76) : ' + uname + "pass : " + passwd});
+
 
 def changePasswd(request):
 	return render_to_response('login/resetPasswd.html', {'Err' : ''})
